5fd154fb5189eeaf5a019dc5e9adddc2fef1f4e6
Mercury <kansamas@gmail.com>
2024-07-28 14:07:27 +0300

    Update TemplaterConfig.cs
    - Added editor folder

--- Editor/TemplaterConfig.cs
+++ Editor/TemplaterConfig.cs
@@ -5,6 +5,6 @@ namespace Tools.Editor.Templater
         public const string TemplaterPath = "Leo's Tools/Templates";
         public const string TemplaterProjectSettingsPath = "ProjectSettings/LeosTemplaterSettings.asset";
         public const string TemplaterGlobalProjectSettingsPath = "ProjectSettings/LeosTemplaterGlobalSettings.asset";
-        public const string TemplaterGeneratedPath = "Assets/Templater";
+        public const string TemplaterGeneratedPath = "Assets/Templater/Editor";
     }
 }
